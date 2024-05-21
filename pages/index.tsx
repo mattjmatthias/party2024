abd735669967f1507634cf6926ed0d7ab62797ae
Matt Matthias <mattjmatthias@hotmail.com>
2024-05-21 16:41:36 +0100

    Swapped the date and time around at the top

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,11 +19,10 @@ const LandingPage: NextPage = () => {
       <section className="self-stretch flex flex-row items-start justify-center p-[80px] mq825:p-[50px] box-border bg-whitesmoke-200 max-w-full text-center text-10xl text-black font-futura lg:box-border">
         <div className="flex flex-col items-start justify-start gap-[24px] max-w-full">
           <b className="self-stretch relative mq450:text-3xl mq825:text-10xl font-extrabold">
-            6PM-LATE, SAT 14TH SEPTEMBER 2024
+            SAT 14TH SEPTEMBER 2024, 6PM-LATE
           </b>
           <div className="self-stretch relative text-3xl leading-[150%] tracking-[-0.75px] font-small-text text-darkslategray-200 mq450:text-lgi mq450:leading-[29px]">
-            It’s Matt + Steve’s 45th birthday this year, so we’re throwing a
-            party to celebrate!
+            It’s Matt + Steve’s 45th birthday this year, so we’re throwing a party to celebrate!
           </div>
         </div>
       </section>
@@ -31,8 +30,7 @@ const LandingPage: NextPage = () => {
       <EighteenIntro2 />
       <div className="self-stretch flex flex-row items-end justify-end pt-0 pb-2.5 pr-5 box-border max-w-full">
         <div className="relative leading-[150%] flex items-end shrink-0 max-w-full tracking-[-0.2px] font-small-text text-right">
-          *Vegan and vegetarian options will be available, and allergies
-          accomodated
+          *Vegan and vegetarian options will be available, and allergies accomodated
         </div>
       </div>
       <section className="w-auto overflow-x-auto flex flex-row items-start justify-start max-w-full">
